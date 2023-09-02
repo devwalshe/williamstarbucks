a32c07dab52229e6f4439cb4dc8751265bc658fb
devwalshe <wwalsh0615@gmail.com>
2023-09-02 19:16:08 -0400

    finally fixed routing issue? hopefully...

--- server.js
+++ server.js
@@ -1,29 +1,31 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+const CORS = require('cors');
 
+app.use(express.static('public'));
+app.use('/PartnerHours', express.static(__dirname + '/public/PartnerHours'));
+app.use('/Menu', express.static(__dirname + '/public/Menu'));
+app.use('/coreDrinks', express.static(__dirname + '/public/CoreDrinks'));
+app.use(express.static('IMG'));
 
-const express = require('express')
-const app = express()
-const PORT = 8000
-const CORS = require('cors')
-app.use(express.static('public'))
-app.use('/PartnerHours',express.static(__dirname + '/public/PartnerHours'))
-app.use('/Menu',express.static(__dirname + '/public/Menu'))
-app.use('/coreDrinks',express.static(__dirname + '/public/CoreDrinks'))
-app.use(express.static('IMG'))
-const bodyParser= require('body-parser')
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(CORS())
-app.use(express.urlencoded({ extended: true }))
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(CORS());
+app.use(express.urlencoded({ extended: true }));
 app.use('/IMG', express.static(__dirname + '/IMG'));
-app.use(express.json())
+app.use(express.json());
 
+// Add this code snippet to allow CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 
-const APIRoutes = require('./routes/API')
-const mainRoutes = require('./routes/main')
+const APIRoutes = require('./routes/API');
+const mainRoutes = require('./routes/main');
 
-app.use('/api',APIRoutes)
-app.use('/',mainRoutes)
+app.use('/api', APIRoutes);
+app.use('/', mainRoutes);
 
-app.listen(process.env.PORT || PORT)
-
-
-
+app.listen(process.env.PORT || PORT);
