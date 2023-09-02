e52e8e4c638fff3e0f763f006a3875fce018a03e
devwalshe <wwalsh0615@gmail.com>
2023-09-02 19:20:45 -0400

    attempt#124245 on fixing routing LOL

--- routes/API.js
+++ routes/API.js
@@ -1,15 +1,33 @@
-const express = require('express')
-const router = express.Router()
-const drinkController = require('../controllers/drinkController')
-const customerController = require('../controllers/customerController')
-const customizeController = require('../controllers/customizeController')
-const roastsController = require('../controllers/roastsController')
+const express = require('express');
+const router = express.Router();
+const fetch = require('node-fetch'); // Import fetch if not already done
+const API_BASE_URL = 'https://williampos-3f06192ea47f.herokuapp.com/';
 
+const drinkController = require('../controllers/drinkController');
+const customerController = require('../controllers/customerController');
+const customizeController = require('../controllers/customizeController');
+const roastsController = require('../controllers/roastsController');
 
-router.get('/coreDrinks', drinkController.allDrinks) 
-router.get('/customers', customerController.sendCustomer) 
-router.get('/allcustomers', customerController.sendAllCustomers) 
-router.get('/allnames', customerController.sendNames) 
-router.get('/customizations', customizeController.sendCustomizations) 
-router.get('/roasts', roastsController.sendRoasts) 
-module.exports = router
+router.get('/coreDrinks', (req, res) => {
+  // Construct the API URL for the 'coreDrinks' endpoint
+  const apiUrl = `${API_BASE_URL}api/coredrinks`;
+
+  // Make the API request using fetch or another HTTP library
+  fetch(apiUrl)
+    .then(response => {
+      // Handle the API response here
+      res.json(response);
+    })
+    .catch(error => {
+      // Handle errors here
+      res.status(500).json({ error: 'An error occurred while fetching data from the API.' });
+    });
+});
+
+router.get('/customers', customerController.sendCustomer);
+router.get('/allcustomers', customerController.sendAllCustomers);
+router.get('/allnames', customerController.sendNames);
+router.get('/customizations', customizeController.sendCustomizations);
+router.get('/roasts', roastsController.sendRoasts);
+
+module.exports = router;
--- routes/main.js
+++ routes/main.js
@@ -1,10 +1,17 @@
-const express = require('express')
-const router = express.Router()
-const mainController = require('../controllers/mainController')
-router.get('/', mainController.index) 
-router.get('/roasts', mainController.roasts) 
-router.get('/pos', mainController.menu) 
-router.get('/coredrinks', mainController.coredrinks) 
-router.post('/order', mainController.order) 
-router.post('/partnerhours', mainController.partnerHours) 
-module.exports = router
+const express = require('express');
+const router = express.Router();
+const mainController = require('../controllers/mainController');
+const apiRoutes = require('./api'); // Import your API routes defined in api.js
+
+// Include the API routes
+router.use('/api', apiRoutes);
+
+// Define your main application routes
+router.get('/', mainController.index);
+router.get('/roasts', mainController.roasts);
+router.get('/pos', mainController.menu);
+router.get('/coredrinks', mainController.coredrinks);
+router.post('/order', mainController.order);
+router.post('/partnerhours', mainController.partnerHours);
+
+module.exports = router;
